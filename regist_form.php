29313b92ad9fbaed3b02e8bdaf65b4ce0150dfcf
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-02-02 14:00:45 +0900

    last setting

---
+++ regist_form.php
@@ -0,0 +1,10 @@
+<form action="register.php" method="post">
+<input type="hidden" name= "mode" value="email_regist" />
+
+<h1>登録フォーム</h1>
+</br>
+id:
+<input type="text" name="email" maxlength="100">
+password:
+<input type="password" name="password" maxlength="256">
+<input type="submit" name="submit" value="aaaa">
