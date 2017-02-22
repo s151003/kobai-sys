2762f18fe6ed9234fa89a0e4614c6583b06e6428
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-02-22 13:24:05 +0900

    2

---
+++ reservation02.php
@@ -0,0 +1,30 @@
+<?php
+$link = mysql_connect('localhost','root','');
+if(!$link){
+	die('接続失敗'.mysql_error());
+}
+$select = mysql_select_db('kobai',$link);
+if(!$select){
+	die('データベース選択失敗'.mysql_error());
+}
+
+$a = $_GET['a'];
+$b = 1;
+while($b <= $a){
+	if(isset($_GET[$b])){
+		echo $b;
+		$sql="delete from reservation02 where id = $b";
+		if(!$res=mysql_query($sql)){
+			echo "エラー";
+			exit;
+		}
+		print('<br>');
+	}else{
+		echo $b;
+		echo '削除';
+		print ('<br>');
+	}
+	$b++;
+}
+
+?>
