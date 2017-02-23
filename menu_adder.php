732c8124d504841efaf21cac2dd30f77014e7f8c
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-02-23 19:20:57 +0900

    Revert "Revert "youbi discount""
    This reverts commit 855572de73d4c924dd0f1f1311678319cd064d6b.

--- menu_adder.php
+++ menu_adder.php
@@ -1,13 +1,27 @@
 <?php
 $product = $_POST['product'];
 $value = $_POST['value'];
+$discount = $_POST['discount'];
+$dis_value = $_POST['dis_value'];
+$after_discount = $value - $dis_value;
 
 require("connect_sql.php");
 
+$week = array("日", "月", "火", "水", "木", "金", "土");
 
-$sql = mysqli_query($link,"INSERT INTO products (name,value) VALUES ('$product','$value')");
-if(!$sql){
-	echo "データベース登録の際にエラー";
-}else{
-	echo "$product を$value 円で登録しました";
-}
+if(!$discount == 7 || $dis_value == ""){  //もし$discountが"7"でないなら
+			echo "いくら割引するか書いてください。";
+	}else{
+		$sql = mysqli_query($link,"INSERT INTO products (name,value,discount,dis_value) VALUES ('$product','$value','$discount','$dis_value')");
+		if(!$sql){
+			echo "データベース登録の際にエラー";
+		}else{
+			echo "$product を価格$value 円で登録しました";
+			echo "</br>";
+			if($discount == 7){
+				echo "割引曜日は設定されていません。";
+			}else{
+				echo "$week[$discount]曜日に$dis_value 円割引します。</br>割引後の価格は$after_discount 円です。";
+			}
+		}
+	}
