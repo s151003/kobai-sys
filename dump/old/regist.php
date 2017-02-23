e7beb0725e3a65b8521d7bec0c34b4da1a1a900b
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-02-23 14:06:43 +0900

    seiri

---
+++ dump/old/regist.php
@@ -0,0 +1,24 @@
+<?php
+/* MySQL接続 */
+$link = mysqli_connect('localhost','root','','kobai');
+
+/* MySQL接続チェック 接続成功ならconnected */
+if(!$link){
+	die('disconnected'. mysql_error());
+}
+echo 'connected</br>';
+
+/* HTMLから入力してPOSTしたデータを受け取る */
+if ($_POST){
+	$id = $_POST['id'];
+	$pass = $_POST['pass'];
+}
+
+echo "Your ID is $id";
+
+/* MySQLへデータを追加 */
+mysqli_query($link,"INSERT INTO member (id,password) VALUES('$id','$pass')");
+
+
+mysqli_close($link);
+?>
---
+++ dump/original/login.php
@@ -0,0 +1,10 @@
+<form action="regist.php" method="post">
+
+
+ID:<br/>
+<input type="text" name="id" maxlength="12"> <br/>
+PASS:<br/>
+<input type="password" name="pass" maxlength="32"> <br/>
+<br/>
+<input type="submit" value="enter">
+</form>
