855572de73d4c924dd0f1f1311678319cd064d6b
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-02-23 19:20:53 +0900

    Revert "youbi discount"
    This reverts commit bcf53e47adc322d0a8eeb094bda73a6b156dc93c.

--- add_menu.php
+++ add_menu.php
@@ -6,19 +6,5 @@
 名前</br>
 <input required type="text" name="product" maxlength="100"></br></br>
 値段</br>
-<input required type="number" name="value" maxlength="100"></br></br>
-曜日割引</br>
-<select name="discount">
-<optgroup>
-  <option value="7">割引なし</option>
-  <option value="1">月曜日</option>
-  <option value="2">火曜日</option>
-  <option value="3">水曜日</option>
-  <option value="4">木曜日</option>
-  <option value="5">金曜日</option>
-</optgroup>
-</select></br></br>
-いくら割引ますか（割引がない場合は空欄）</br>
-<input type="number" name="dis_value" maxlength="100">
-</br></br></br>
-<input type="submit" name="submit" value="aaaa">
+<input required type="number" name="value" maxlength="100">
+</br></br><input type="submit" name="submit" value="aaaa">
