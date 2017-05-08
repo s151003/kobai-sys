c47200156549637743184eb2f8670c64e3abe446
s151003 <hogefuga@example.com>
2017-05-08 14:48:23 +0900

    menu_change.php is good

---
+++ mypage/login.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>login</title>
+	</head>
+	<body>
+		<form action="auth.php" method="POST">
+id:
+<input type="text" name="id" maxlength="100">
+password:
+<input type="password" name="password" maxlength="256">
+
+<input type="submit" name="submit" value="login">
