80dabc1b246fc74ae7a58b9c3187b6053c151063
s151003 <hogefuga@example.com>
2017-10-07 18:33:51 +0900

    [Clean]エラーメッセージをしっかり書いた

--- connect_db.php
+++ connect_db.php
@@ -7,7 +7,8 @@ $dbname = "koubai";
 $link = mysqli_connect($hostname, $username, $password, $dbname);
 
 if(!$link){
-	echo "dbconnect error";
+	echo "データベースへ接続する際にエラーが発生しました";
+	exit;
 }else{
 	mysqli_query($link,"set names utf8");
 }
