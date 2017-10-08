90ec8901a35b8b13ed1b64facadef7f4ae7b2e9a
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-08 18:28:39 +0900

    2017/10/08

--- console/menu_change.php
+++ console/menu_change.php
@@ -1,31 +1,33 @@
+<form action="menu_changer.php" method="post">
+
+<!-- 表生成開始 -->
+<table border>
+<tr><td>商品ID</td><td>商品名</td><td>価格</td><td>曜日割引</td><td>割引価格</td><td>削除</td><td></td></tr>";
+
 <?php
-require("../connect_sql.php");
-
-$query = mysqli_query($link,'SELECT id,name,value,dis_day,dis_value FROM products');
-
-// 表開始
-echo "<table border>";
-echo "<tr><td>商品ID</td><td>商品名</td><td>価格</td><td>曜日割引</td><td>割引価格</td><td></td></tr>";
+require("../connect_db.php");
 $week = array("日", "月", "火", "水", "木", "金", "土","なし");
-
+$query = mysqli_query($link,'SELECT id,name,value,dis_day,dis_value FROM products');
 $count = 0;
+
 while ($row = mysqli_fetch_assoc($query)){
   $count ++;
-  $discount = $row["discount"];
 
   echo "<tr>";
   echo "<td>",$count,"</td>"; //商品ID
   echo "<td>",$row["name"],"</td>"; //商品名
   echo "<td>",$row["value"],"</td>"; //価格
-  echo "<td>",$week[$discount],"</td>"; //割引曜日 $week→day.php
+  echo "<td>",$week[$row["dis_day"]],"</td>"; //割引曜日 $week→day.php
   echo "<td>",$row["dis_value"],"</td>";
-  echo "<td><input type='checkbox' name='del' value='".$count."'></td>"; //checkbox
+  echo "<td><input type='submit' name='del$count' value='$count&shy削除'</td>";
+  echo "<td><input type='checkbox' name='edit' value='".$count."'></td>"; //checkbox
   echo "</tr>";
 }
-echo "</table>";
-//表終わり
-
-echo "商品数 $count"; //商品数
-
-echo '<input type="submit" name="button1" value="送信する">';
+  echo "商品数 $count"; //商品数
+  echo "</table>";
 ?>
+<!-- 表ここまで -->
+
+<input type="submit" name="button1" value="送信する">
+
+</form>
