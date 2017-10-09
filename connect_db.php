971458f15d0f555307f6525fa541e5121bb54710
s151003 <hogefuga@example.com>
2017-10-09 14:32:10 +0900

    [Update]bootstrap対応

--- connect_db.php
+++ connect_db.php
@@ -7,7 +7,11 @@ $dbname = "koubai";
 $link = mysqli_connect($hostname, $username, $password, $dbname);
 
 if(!$link){
-	echo "データベースへ接続する際にエラーが発生しました <b>connect_db.php</b>";
+	require("head.php");
+	output("データベース接続エラー");
+	echo '<div class="container">';
+	echo '<h1>データベース接続エラー</h1><hr>';
+	echo '<div class="alert alert-warning" role="alert">データベースへ接続する際にエラーが発生しました <b>connect_db.php</b></div></div>';
 	exit;
 }else{
 	//MySQLで使う文字コードを設定
