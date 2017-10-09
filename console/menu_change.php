3470e31d9250102a070be2a5a4ff3dc3c8b8d8db
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-09 20:38:08 +0900

    [Update]menu_change.php bootstrap対応 削除ボタンで商品削除

--- console/menu_change.php
+++ console/menu_change.php
@@ -1,10 +1,15 @@
 <form action="menu_changer.php" method="post">
-
+<?php
+require("../head.php");
+output("メニュー管理ページ");
+ ?>
 <!-- 表生成開始 -->
-<table border>
-<tr><td>商品ID</td><td>商品名</td><td>価格</td><td>曜日割引</td><td>割引価格</td><td>削除</td><td></td></tr>";
+<div class="container">
+<table class="table">
+<tr><td>#</td><td>商品名</td><td>価格</td><td>曜日割引</td><td>割引価格</td><td>削除</td><td></td><td>予約受付数</td><td>販売受付</td></tr>
 
 <?php
+session_start();
 require("../connect_db.php");
 $week = array("日", "月", "火", "水", "木", "金", "土","なし");
 $query = mysqli_query($link,'SELECT id,name,value,dis_day,dis_value FROM products');
@@ -17,17 +22,19 @@ while ($row = mysqli_fetch_assoc($query)){
   echo "<td>",$count,"</td>"; //商品ID
   echo "<td>",$row["name"],"</td>"; //商品名
   echo "<td>",$row["value"],"</td>"; //価格
-  echo "<td>",$week[$row["dis_day"]],"</td>"; //割引曜日 $week→day.php
+  echo "<td>",$week[$row["dis_day"]],"</td>"; //割引曜日
   echo "<td>",$row["dis_value"],"</td>";
-  echo "<td><input type='submit' name='del$count' value='$count&shy削除'</td>";
+  echo "<td><button class=\"btn btn-default\" type='submit' name='del' value='",$row["name"],"'>削除</button></td>";
   echo "<td><input type='checkbox' name='edit' value='".$count."'></td>"; //checkbox
+  echo "<td>1日 個数</td>";
+  echo "<td>予約</td>";
   echo "</tr>";
 }
-  echo "商品数 $count"; //商品数
   echo "</table>";
+  echo "商品数 $count"; //商品数
 ?>
 <!-- 表ここまで -->
-
-<input type="submit" name="button1" value="送信する">
-
+</br></br>
+<input class="btn btn-default" type="submit" value="送信ボタン">
 </form>
+</div>
--- console/menu_changer.php
+++ console/menu_changer.php
@@ -1,1 +1,10 @@
-require("connect_sql.php");
+<?php
+require("../connect_db.php");
+
+
+if (isset($_POST['del'])){
+  $name = $_POST['del'];
+  $query = "DELETE FROM products WHERE name='$name'";
+  echo $query;
+  mysqli_query($link,"$query");
+};
