54a6eb6423b165d833ca19f6491240e99b18c875
s151003 <hogefuga@example.com>
2017-10-10 16:40:13 +0900

    [Update]connect_db.php localhostだと接続できないので127.0.0.1へ変更

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,11 @@
 <?php
-$hostname = "localhost";
+$hostname = "127.0.0.1";
 $username = "root";
-$password = "";
+$password = "114514";
 $dbname = "koubai";
+
+ini_set("display_errors", On);
+error_reporting(E_ALL);
 
 $link = mysqli_connect($hostname, $username, $password, $dbname);
 
@@ -11,9 +14,10 @@ if(!$link){
 	output("データベース接続エラー");
 	echo '<div class="container">';
 	echo '<h1>データベース接続エラー</h1><hr>';
+	echo mysqli_errno($link).": ".mysqli_error($link);
 	echo '<div class="alert alert-warning" role="alert">データベースへ接続する際にエラーが発生しました <b>connect_db.php</b></div></div>';
 	exit;
 }else{
 	//MySQLで使う文字コードを設定
-	mysql_set_charset('utf8');
+	mysqli_character_set_name($link);
 }
