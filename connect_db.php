d30d43fce99e7108113bc440225c053fde18e362
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-10 16:42:45 +0900

    [Update]connect_db.php
    MySQL接続不能の際のエラーをアラート内に収めた

--- connect_db.php
+++ connect_db.php
@@ -14,8 +14,9 @@ if(!$link){
 	output("データベース接続エラー");
 	echo '<div class="container">';
 	echo '<h1>データベース接続エラー</h1><hr>';
+	echo '<div class="alert alert-warning" role="alert">';
 	echo mysqli_errno($link).": ".mysqli_error($link);
-	echo '<div class="alert alert-warning" role="alert">データベースへ接続する際にエラーが発生しました <b>connect_db.php</b></div></div>';
+	echo 'データベースへ接続する際にエラーが発生しました <b>connect_db.php</b></div></div>';
 	exit;
 }else{
 	//MySQLで使う文字コードを設定
