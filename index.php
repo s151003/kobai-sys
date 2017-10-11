a2d0bfa196d7afe0fbe9e39030dff92b06f29384
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-11 19:30:46 +0900

    [Fix] index.php Bootstrapをhead.phpから読み込む

--- index.php
+++ index.php
@@ -1,36 +1,7 @@
-<!DOCTYPE html>
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<html lang = "ja">
-<head>
-  <title>トップページ</title>
-  <!-- bootstrap読み込み -->
-  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <script src="bootstrap/js/bootstrap.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <!-- bootstrap読み込みここまで -->
-</head>
-<body>
-
-<!-- ナビゲーションバーここから -->
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand">購買予約システム</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active">
-      <li><a href="../kobai-sys/mypage/login.php">ログイン</a></li>
-      <li><a href="../kobai-sys/mypage/regist_form.php">新規登録</a></li>
-      <li><a href="../kobai-sys/console">管理画面</a></li>
-    </ul>
-    <button type="button" class="btn btn-default navbar-btn">
-      <span class="glyphicon glyphicon-envelope"></span>
-    </button>
-  </div>
-</nav>
-<!-- ナビゲーションバーここまで -->
-
+<?php
+  require("head.php");
+  output("トップページ");
+?>
 <body>
   <div class="container">
     <ol class="breadcrumb">
