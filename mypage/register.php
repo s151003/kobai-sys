1538b546eb88a49b554121c53de5a67156ff5afc
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-12 20:51:15 +0900

    Revert "[Fix] 新しいデータベースのカラム名を適用"
    This reverts commit 320939e41f319a01b24f9aaab16ffa557c0d04c5.

--- mypage/register.php
+++ mypage/register.php
@@ -11,13 +11,13 @@ $hash = password_hash("$password", PASSWORD_DEFAULT);
 if($id == "" || $password == ""){
 	print "IDかパスワードが空です";
 }else{
-	$result = mysqli_query($link,"SELECT * FROM `member` WHERE `user_id` = '$id'");
+	$result = mysqli_query($link,"SELECT * FROM `member` WHERE `id` = '$id'");
 	$row = mysqli_fetch_array($result);
 
 	if(in_array($id,$row)){ //$rowの配列のなかに$idがあったら
 		echo "そのIDは既に使われてます";
 	}else{
-		$query = mysqli_query($link,"INSERT INTO member(user_id,password,time) VALUES('$id','$hash','$datetime')");
+		$query = mysqli_query($link,"INSERT INTO member(id,password,time) VALUES('$id','$hash','$datetime')");
 		if(!$query){
 			echo "データベースへ登録する際にエラーが発生したため登録できませんでした";
 		}else{
