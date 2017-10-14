7af91d81361674798edc3e022d6a1438d7589977
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-14 17:28:09 +0900

    [Delete] add_menu.php add_menu.phpへ名前変更
    統一

--- console/menu_add.php
+++ console/menu_add.php
@@ -1,24 +1,66 @@
+<head>
+  <?php
+  require("../head.php");
+  output("メニュー追加フォーム");
+
+  $category = array("菓子パン","惣菜パン","食パン");
+  ?>
 <form action="menu_adder.php" method="post">
 <input type="hidden" name= "mode" value="menu_adder" />
 
-<title>メニュー管理ページ</title>
-    <h2>メニュー追加</h2>
-名前</br>
-<input required type="text" name="product" maxlength="100"></br></br>
-値段</br>
-<input required type="number" name="value" maxlength="100"></br></br>
-曜日割引</br>
-<select name="discount">
-<optgroup>
-  <option value="7">割引なし</option>
-  <option value="1">月曜日</option>
-  <option value="2">火曜日</option>
-  <option value="3">水曜日</option>
-  <option value="4">木曜日</option>
-  <option value="5">金曜日</option>
-</optgroup>
-</select></br></br>
-いくら割引ますか（割引がない場合は空欄）</br>
-<input type="number" name="dis_value" maxlength="100">
-</br></br></br>
-<input type="submit" name="submit" value="aaaa">
+<div class="container">
+  <div class="col-md-6">
+  <h2>メニュー追加</h2>
+  <hr>
+
+  <div class="form-froup">
+    <label>名前</br></label>
+    <input required type="text" name="product" maxlength="100" class="form-control"></br>
+  </div>
+  <div class="form-froup">
+    <label>値段</label></br>
+    <input required type="number" name="value" maxlength="100" class="form-control"></br>
+  </div>
+  <div class="form-froup">
+    <label>カテゴリー</label></br>
+    <select name="category" class="form-control">
+    <optgroup>
+      <?php
+      $i = 0;
+      while ($i < count($category)) {
+        echo "<option value=",$i,">",$category[$i],"</option>";
+        $i ++;
+      }
+      ?>
+    </optgroup>
+  </select></br>
+  </div>
+  <div class="form-froup">
+    <label>曜日割引</label></br>
+    <select name="discount" class="form-control">
+    <optgroup>
+      <option value="7">割引なし</option>
+      <option value="1">月曜日</option>
+      <option value="2">火曜日</option>
+      <option value="3">水曜日</option>
+      <option value="4">木曜日</option>
+      <option value="5">金曜日</option>
+    </optgroup>
+    </select></br>
+  </div>
+  <div class="form-froup">
+    <label>いくら割引ますか（割引がない場合は空欄）</label></br>
+    <input type="number" name="dis_value" maxlength="100" class="form-control"></br>
+  </div>
+  <div class="form-froup">
+    <label>一日あたりの販売個数</label></br>
+    <input required type="number" name="day_limit" maxlength="100" class="form-control"></br>
+  </div>
+  <div class="form-froup">
+    <label>ひとことコメント</br></label>
+    <input required type="text" name="comment" maxlength="100" class="form-control"></br>
+  </div>
+    <input class="btn btn-primary" type="submit" name="submit" value="送信">
+  </div>
+    <div class="col-md-6"></div>
+</div>
