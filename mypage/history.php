cf956e346009362e10880c1a481ad408362e5466
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-14 17:04:16 +0900

    [Add] 表をDataTableを使って表示

--- mypage/history.php
+++ mypage/history.php
@@ -10,23 +10,24 @@
   $row = mysqli_fetch_assoc($result);
   $id = $row["id"];
   $status = array("受付","受け取り済み","問題");
+  DataTable("history");
   ?>
 
   <div class="container">
       <h1>あなたの予約履歴</h1>
       <hr>
-  <table class="table">
-  <tr><td>#</td><td>商品名</td><td>合計金額</td><td>状態</td><td>日付</td></tr>
+  <table id="history" class="table table-striped table-bordered cellspacing="0"">
+  <thead><tr><td>#</td><td>商品名</td><td>合計金額</td><td>状態</td><td>日付</td></tr></thead>
 <?php
   $query = "SELECT * FROM `yoyaku` WHERE `user_id`= '$id'";
   $result = mysqli_query($link, $query);
   while ($row = mysqli_fetch_assoc($result)){
     echo "<tr>";
-    echo "<td>",$row["id"],"</td>"; //予約ID
+    echo "<td>".$row["id"]."</td>"; //予約ID
     echo "<td>商品名</td>"; //商品名
     echo "<td>価格</td>"; //価格
-    echo "<td>",$row["date"],"</td>"; //checkbox
-    echo "<td>",$status[$row["status"]],"</td>";
+    echo "<td>".$row["date"]."</td>"; //checkbox
+    echo "<td>".$status[$row["status"]]."</td>";
     echo "</tr>";
   };
 ?>
