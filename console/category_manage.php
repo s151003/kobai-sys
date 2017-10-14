c68ac4be372a0cb46bbf5940af328722a318aec0
s151003 <hogefuga@example.com>
2017-10-14 19:18:47 +0900

    [Add] 商品カテゴリーを管理するページを追加
    カテゴリー、コメントを追加できる
    削除もできる

---
+++ console/category_manage.php
@@ -0,0 +1,38 @@
+<?php
+
+require("../connect_db.php");
+require("../head.php");
+output("カテゴリー管理ページ");
+DataTable("category");
+?>
+<form action="category_manager.php" method="post">
+<div class="container">
+  <h2>カテゴリー追加</h2>
+  <hr>
+<div class="form-froup">
+  <label>カテゴリー名</br></label>
+  <input type="text" name="name" maxlength="100" class="form-control"></br>
+</div>
+<div class="form-froup">
+  <label>コメント</br></label>
+  <input type="text" name="comment" maxlength="100" class="form-control"></br>
+</div>
+  <input class="btn btn-primary" type="submit" name="submit" value="送信">
+</br></br>
+
+<table id="category" class="table table-striped table-bordered cellspacing="0"">
+<thead><tr><td>#</td><td>カテゴリー</td><td>コメント</td><td>削除</td></tr></thead>
+<?php
+$query = "SELECT * FROM category";
+$result = mysqli_query($link, $query);
+while ($row = mysqli_fetch_assoc($result)){
+  echo "<tr>";
+  echo "<td>".$row["id"]."</td>"; //予約ID
+  echo "<td>".$row["name"]."</td>"; //商品名
+  echo "<td>".$row["comment"]."</td>"; //価格
+  echo "<td><button class=\"btn btn-default\" type='submit' name='del' value='",$row["id"],"'>削除</button></td>";
+  echo "</tr>";
+};
+
+?>
+</div>
---
+++ console/category_manager.php
@@ -0,0 +1,28 @@
+<?php
+require("../connect_db.php");
+require("../head.php");
+output("カテゴリー追加");
+
+if (!$_POST['name']==""){
+  $name = $_POST['name'];
+  $comment = $_POST['comment'];
+  $result = mysqli_query($link,"INSERT INTO category (name,comment) VALUES ('$name','$comment')");
+  echo "<div class=\"container\">";
+
+  if(!$result){
+    echo "Errormessage: ", mysqli_error($link),"</br>";
+  }else{
+    echo "<div class=\"alert alert-success\" role=\"alert\"><strong>カテゴリー追加成功</strong></br>";
+    echo "カテゴリー名：".$name."</br>";
+    echo "コメント：".$comment."</div>";
+  }
+  echo "</div>";
+}
+if (isset($_POST['del'])){
+  $id = $_POST['del'];
+  echo $id;
+  $query = "DELETE FROM category WHERE id='$id'";
+  mysqli_query($link,"$query");
+};
+
+?>
