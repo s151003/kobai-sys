d5191c453db71a633677b5d47b7826fa6a5d4429
s151003 <hogefuga@example.com>
2017-10-14 15:15:54 +0900

    [Update] ログインしているとnavbar左に「ようこそ●●さん」と表示されるように
    非ログイン状態だとログイン&登録ボタン

--- head.php
+++ head.php
@@ -2,17 +2,20 @@
 
 //bootstrapを読み込みヘッダーを生成する
 function output($title){
+      session_start();
+
+
       echo <<<EOM
       <!DOCTYPE html>
       <meta charset="utf-8">
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <html lang = "ja">
       <head>
         <title> $title </title>
-        <!-- bootstrap読み込み -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
         <!-- bootstrap読み込みここまで -->
       </head>
     <body>
@@ -29,11 +32,18 @@ function output($title){
             <li><a href="/kobai-sys/console">管理画面</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="/kobai-sys/mypage/regist_form.php"><span class="glyphicon glyphicon-user"></span> 登録</a></li>
-            <li><a href="/kobai-sys/mypage/login.php"><span class="glyphicon glyphicon-log-in"></span> ログイン</a></li>
+EOM;
+          if (isset($_SESSION['sid'])){
+            echo "<p class=\"navbar-text\">ようこそ！".$_SESSION['sid']."さん</p>";
+          }else {
+            echo "<li><a href=\"/kobai-sys/mypage/regist_form.php\"><span class=\"glyphicon glyphicon-user\"></span> 登録</a></li>";
+            echo "<li><a href=\"/kobai-sys/mypage/login.php\"><span class=\"glyphicon glyphicon-log-in\"></span> ログイン</a></li>";
+          }
+echo <<<EOM
           </ul>
         </div>
       </nav>
       <!-- ナビゲーションバーここまで -->
 EOM;
+
   }
