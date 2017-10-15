febd58605edbc40c173368f92ec43648c1017291
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-15 20:14:56 +0900

    [Update] koubai.sql
    ダミー商品データ、カテゴリーテーブル

--- koubai.sql
+++ koubai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017 年 10 朁E09 日 18:56
+-- Generation Time: 2017 年 10 朁E15 日 13:13
 -- サーバのバージョン： 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -23,6 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `comment`) VALUES
+(1, '惣菜パン', 'お昼にどうぞ'),
+(2, '菓子パン', 'おやつにどうぞ'),
+(3, 'その他', 'おいしいパン'),
+(4, 'マフィン', 'おいしい'),
+(8, 'サンドウィッチ', 'ハムサンドがオススメ');
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `member`
 --
 
@@ -32,6 +55,17 @@ CREATE TABLE `member` (
   `password` varchar(255) NOT NULL,
   `time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `member`
+--
+
+INSERT INTO `member` (`id`, `user_id`, `password`, `time`) VALUES
+(6, 'aki', '$2y$10$X7uOG/R2A1y8hmp3CK9mAuGd76ENtOPxlOZh80Lb5BO0PZOy1gdjC', '2017-10-12 12:20:06'),
+(7, '1', '$2y$10$Rimyj7grCL0lOxGAL.aUpukSfYlLRyjrHBsnhiJRlGo3Aprxgw3Vq', '2017-10-12 12:25:31'),
+(8, '123', '$2y$10$AtTNXlD3xe5uZTbTNg53/ebYJ4Qq/WEhmyqZbFXQcNHlhaQHPfd4y', '2017-10-12 12:27:50'),
+(9, '12345', '$2y$10$G2S1R2Zm2TxQLSbIFBubCu6IlVx0N5wXcbx5o4guCQmYB3SndnJie', '2017-10-14 06:34:02'),
+(10, '123456', '$2y$10$6tOn/QVlCbVsa2D/ui/J8.Dw79FZn08sead9IB5lAp2o3d57pFY6e', '2017-10-14 06:34:22');
 
 -- --------------------------------------------------------
 
@@ -56,11 +90,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `value`, `dis_day`, `dis_value`, `day_limit`, `status`, `category`, `comment`) VALUES
-(3, 'カレーパン（辛口）', 80, 7, 0, 0, 0, 0, ''),
-(4, 'カレーパン（甘口）', 80, 7, 0, 0, 0, 0, ''),
-(5, 'メロンパン', 80, 7, 0, 0, 0, 0, ''),
-(6, 'モーニングコーヒー', 90, 7, 0, 0, 0, 0, ''),
-(7, 'ラスク', 100, 7, 0, 0, 0, 0, '');
+(3, 'カレーパン（辛口）', 80, 7, 0, 20, 0, 1, '辛いカレーパンで眠気覚まし'),
+(4, 'カレーパン（甘口）', 80, 7, 0, 0, 0, 1, '辛いものが苦手な方はこちら'),
+(6, 'モーニングコーヒー', 90, 7, 0, 0, 0, 2, '生クリームとコーヒークリームがマッチ！'),
+(7, 'ラスク', 50, 7, 0, 0, 0, 3, '牛乳を飲みながら食べたい'),
+(8, '食パン', 120, 4, 15, 60, 0, 3, 'ジャムをお持ちの方はこちらをどうぞ'),
+(9, 'くるみパン', 120, 3, 50, 30, 0, 1, 'くるみがおいしい'),
+(11, 'メロンパン', 100, 2, 10, 20, 0, 1, '外はサクサク。中はしっとり。');
 
 -- --------------------------------------------------------
 
@@ -70,15 +106,32 @@ INSERT INTO `products` (`id`, `name`, `value`, `dis_day`, `dis_value`, `day_limi
 
 CREATE TABLE `yoyaku` (
   `id` int(255) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
-  `products` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `products` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
   `date` datetime NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- テーブルのデータのダンプ `yoyaku`
+--
+
+INSERT INTO `yoyaku` (`id`, `user_id`, `products`, `quantity`, `date`, `status`) VALUES
+(1000, 6, '3', '1', '2017-10-28 06:29:48', '1'),
+(1001, 6, '4', '8', '2017-10-13 00:00:00', '2');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
 
 --
 -- Indexes for table `member`
@@ -107,20 +160,25 @@ ALTER TABLE `yoyaku`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `yoyaku`
 --
 ALTER TABLE `yoyaku`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1002;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
