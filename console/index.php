b3ac8e212df43dff67be5c10203e23f01fee4126
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-20 00:38:56 +0900

    [Update] 仮 管理メニュー

--- console/index.php
+++ console/index.php
@@ -2,8 +2,9 @@
 
 <title>メニュー管理ページ</title>
     <h1>メニュー管理ページ</h1></br>
-<a href="add_menu.php">メニュー追加</a></br></br>
-<a href="mod_menu.php">メニュー修正</a>
+<a href="menu_add.php">メニュー追加</a></br></br>
+<a href="menu_change.php">メニュー管理</a></br></br>
+<a href="category_manage.php">カテゴリー管理</a>
 
 
 <?php
