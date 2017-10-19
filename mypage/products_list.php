1134a35a0096ad18df4bdef6cf43239186a3c795
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-20 00:12:15 +0900

    [Update] レイアウトが崩れないように画像の縦幅を固定

--- mypage/products_list.php
+++ mypage/products_list.php
@@ -3,7 +3,14 @@
 	require("../head.php");
 	output("商品ページ");
 	$cat = $_GET['cat'];
-
+	?>
+	<!-- レイアウトが崩れないように画像の縦幅を固定 -->
+	<style type="text/css">
+	img {
+	  max-height: 250px;
+	}
+	</style>
+<?php
 	$query = mysqli_query($link,"SELECT * FROM `products` WHERE `category` = '$cat'");
 	echo "<div class=\"container\">";
 	echo "<div class=\"row\">";
@@ -12,13 +19,11 @@
 		$comment = $row['comment'];
 		$id = $row['id'];
 		$img = "../imgs/".$row['id'].".jpg";
-
 		//画像がなかったら$imgをダミー画像に
 		if(!$get_contents = @file_get_contents($img)){
 			$img = "../imgs/not_found.jpg";
 		}
 		ProductCard("$name","$comment","$id","$img");
-    }
-      echo "</div>";
+		}
+    echo "</div>";
 	  echo "</div>";
-?>
