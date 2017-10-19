a47d66735a40284fec7070ae1ce7a08308173817
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-20 00:17:29 +0900

    [Clean] 整理

--- mypage/products_list.php
+++ mypage/products_list.php
@@ -3,7 +3,7 @@
 	require("../head.php");
 	output("商品ページ");
 	$cat = $_GET['cat'];
-	?>
+?>
 	<!-- レイアウトが崩れないように画像の縦幅を固定 -->
 	<style type="text/css">
 	img {
@@ -14,7 +14,7 @@
 	$query = mysqli_query($link,"SELECT * FROM `products` WHERE `category` = '$cat'");
 	echo "<div class=\"container\">";
 	echo "<div class=\"row\">";
-    while ($row = mysqli_fetch_assoc($query)){
+  while ($row = mysqli_fetch_assoc($query)){
 		$name = $row['name'];
 		$comment = $row['comment'];
 		$id = $row['id'];
@@ -24,6 +24,6 @@
 			$img = "../imgs/not_found.jpg";
 		}
 		ProductCard("$name","$comment","$id","$img");
-		}
-    echo "</div>";
-	  echo "</div>";
+	}
+  echo "</div>";
+	echo "</div>";
