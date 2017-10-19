656002f88ed3af6dee845ae619d2a4e1886a85bc
s151003 <hogefuga@example.com>
2017-10-19 23:12:27 +0900

    [:tada:] 商品画像を表示
    .jpg以外対応できない #11

--- mypage/products_list.php
+++ mypage/products_list.php
@@ -3,7 +3,7 @@
 	require("../head.php");
 	output("商品ページ");
 	$cat = $_GET['cat'];
-	
+
 	$query = mysqli_query($link,"SELECT * FROM `products` WHERE `category` = '$cat'");
 	echo "<div class=\"container\">";
 	echo "<div class=\"row\">";
@@ -11,7 +11,13 @@
 		$name = $row['name'];
 		$comment = $row['comment'];
 		$id = $row['id'];
-		CategoryCard("$name","$comment","$id");
+		$img = "../imgs/".$row['id'].".jpg";
+
+		//画像がなかったら$imgをダミー画像に
+		if(!$get_contents = @file_get_contents($img)){
+			$img = "../imgs/not_found.jpg";
+		}
+		ProductCard("$name","$comment","$id","$img");
     }
       echo "</div>";
 	  echo "</div>";
