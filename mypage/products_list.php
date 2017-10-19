cc0a47e88569bd436698d5b80318960a7321a48c
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-19 22:03:15 +0900

    [:tada:] GETで受け取ったカテゴリーの商品を表示

--- mypage/products_list.php
+++ mypage/products_list.php
@@ -1,5 +1,18 @@
 <?php
-  echo $_GET['cat'];
-
-
+	require("../connect_db.php");
+	require("../head.php");
+	output("商品ページ");
+	$cat = $_GET['cat'];
+	
+	$query = mysqli_query($link,"SELECT * FROM `products` WHERE `category` = '$cat'");
+	echo "<div class=\"container\">";
+	echo "<div class=\"row\">";
+    while ($row = mysqli_fetch_assoc($query)){
+		$name = $row['name'];
+		$comment = $row['comment'];
+		$id = $row['id'];
+		CategoryCard("$name","$comment","$id");
+    }
+      echo "</div>";
+	  echo "</div>";
 ?>
