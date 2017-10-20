2d7891894a1dfee746632dfac75c7d5c5a56f8fe
s151003 <s151003@kashiwanoha.ed.jp>
2017-10-20 12:54:00 +0900

    [Update] Bootstrap適用 Resolved #12

--- console/index.php
+++ console/index.php
@@ -1,10 +1,21 @@
+<?php
+  require("../head.php");
+  output("メニュー管理ページ");
+?>
+    <div class = "container">
+    <h1>メニュー管理ページ</h1><hr>
 
 
-<title>メニュー管理ページ</title>
-    <h1>メニュー管理ページ</h1></br>
-<a href="menu_add.php">メニュー追加</a></br></br>
-<a href="menu_change.php">メニュー管理</a></br></br>
-<a href="category_manage.php">カテゴリー管理</a>
 
-
+<div class="panel panel-default">
+	<div class="panel-heading">
+		console
+	</div>
+	<div class="panel-body">
+    <a class="btn btn-primary" href="menu_add.php">メニュー追加</a></br></br>
+    <a class="btn btn-primary" href="menu_change.php">メニュー管理</a></br></br>
+    <a class="btn btn-primary" href="category_manage.php">カテゴリー管理</a>
+	</div>
+</div>
+</div>
 <?php
