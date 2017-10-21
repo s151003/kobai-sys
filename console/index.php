26d108ea8c48de31e96b3085df054591bea581b7
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-21 19:30:09 +0900

    [Clean] 整理

--- console/index.php
+++ console/index.php
@@ -5,17 +5,8 @@
     <div class = "container">
     <h1>メニュー管理ページ</h1><hr>
 
-
-
-<div class="panel panel-default">
-	<div class="panel-heading">
-		console
-	</div>
-	<div class="panel-body">
     <a class="btn btn-primary" href="menu_add.php">メニュー追加</a></br></br>
     <a class="btn btn-primary" href="menu_change.php">メニュー管理</a></br></br>
     <a class="btn btn-primary" href="category_manage.php">カテゴリー管理</a>
-	</div>
-</div>
-</div>
+
 <?php
