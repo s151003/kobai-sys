b02b9794d4192d5ac24044677578e924b33a80a1
s151003 <hogefuga@example.com>
2017-10-21 20:59:08 +0900

    [Update] ProductCard()をproducts_list.phpへ移動
    ここでしか使わないから

--- mypage/products_list.php
+++ mypage/products_list.php
@@ -8,7 +8,7 @@
 	<!-- レイアウトが崩れないように画像の縦幅を固定 -->
 	<style type="text/css">
 	img {
-	  max-height: 250px;
+	  max-height: 200px;
 	}
 	</style>
 <?php
@@ -19,12 +19,35 @@
 		$name = $row['name'];
 		$comment = $row['comment'];
 		$id = $row['id'];
+		$value = $row['value'];
 		$img = "../imgs/".$row['id'].".jpg";
 		//画像がなかったら$imgをダミー画像に
 		if(!$get_contents = @file_get_contents($img)){
 			$img = "../imgs/not_found.jpg";
 		}
-		ProductCard("$name","$comment","$id","$img");
+		ProductCard("$name","$comment","$id","$img","$value");
 	}
   echo "</div>";
 	echo "</div>";
+
+function ProductCard($name,$comment,$id,$img,$value){
+	  echo <<<EOM
+		<div class="col-md-4">
+	  <div class="panel panel-primary">
+	  <div class="panel-heading">$name</div>
+	  <div class="panel-body">
+	    <div class="thumbnail">
+	      <img src="$img">
+	    </div>
+
+	    <h3>$name</h3>
+	    <p>$comment</p>
+	    <Div Align="right"></div>
+	    </div>
+	    <div class="panel-footer">
+	    <font size="5"><b><span class="glyphicon glyphicon-yen aria-hidden="true"></span>$value</b></font><Div Align="right"><a href="product.php?id=$id" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 追加</a></p></font></div></div>
+		</div>
+	  </div>
+	EOM;
+	}
+	?>
