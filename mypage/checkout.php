54f2502427467f4633ebff5408994d514129f14b
s151003 <hogefuga@example.com>
2017-10-23 02:16:31 +0900

    [Update] ログイン認証

--- mypage/checkout.php
+++ mypage/checkout.php
@@ -2,6 +2,8 @@
 require("../connect_db.php");
 require("../head.php");
 output("予約確定ページ");
+LoginVerify($_SESSION['sid']);
+
 $userid = $_SESSION['sid'];
 $query = "SELECT id FROM member WHERE user_id = '$userid'";
 $result = mysqli_query($link,$query);
@@ -11,7 +13,7 @@ $id = $row['id'];
 if(isset($_SESSION['cart'][$userid])){
   foreach($_SESSION['cart'][$userid] as $key => $value){
     $datetime = date("Y-m-d H:i:s");
-    $query = "INSERT INTO yoyaku (user_id, product, quantity, date) VALUES ('$id', '$value[0]', '$value[1]','')";
+    $query = "INSERT INTO yoyaku (user_id, product, quantity, date) VALUES ('$id', '$value[0]', '$value[1]','$datetime')";
     $result = mysqli_query($link,$query);
   };
   unset($_SESSION['cart']);
