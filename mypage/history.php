4462de01cde297e3cae6fdece267a4602c6fcf00
s151003 <hogefuga@example.com>
2017-10-23 03:34:23 +0900

    [Update]

--- mypage/history.php
+++ mypage/history.php
@@ -17,20 +17,34 @@
       <h1>あなたの予約履歴</h1>
       <hr>
   <table id="history" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0">
-  <thead><tr><td>#</td><td>商品名</td><td>金額</td><td>状態</td><td>日付</td></tr></thead>
+  <thead><tr><td>#</td><td>商品名</td><td>金額</td><td>個数</td><td>状態</td><td>日付</td></tr></thead>
 <?php
   $query = "SELECT * FROM `yoyaku` WHERE `user_id`= '$id'";
-
   $result = mysqli_query($link, $query);
+
   while ($row = mysqli_fetch_assoc($result)){
+    $return = NameValue($row['product']);
     echo "<tr>";
     echo "<td>".$row["id"]."</td>"; //予約ID
-    echo "<td>商品名</td>"; //商品名
-    echo "<td>価格</td>"; //価格
+    echo "<td>".$return[0]."</td>"; //商品名
+    echo "<td>".$row['quantity']."</td>";
+    echo "<td>".$return[1] * $row['quantity']."</td>"; //価格
+
     echo "<td>".$status[$row["status"]]."</td>"; //checkbox
     echo "<td>".$row["date"]."</td>";
     echo "</tr>";
   };
+
+  function NameValue($id){
+    require("../connect_db.php");
+    $query = "SELECT name,value FROM products WHERE id = '$id'";
+    $result = mysqli_query($link,$query);
+    $row = mysqli_fetch_assoc($result);
+    $name = $row['name'];
+    $value = $row['value'];
+
+    return array($row['name'],$row['value']);
+  }
 ?>
   </table>
 </div>
