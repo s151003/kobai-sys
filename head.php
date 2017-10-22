4734be0877de165a099404558cc7875cd75ba629
s151003 <hogefuga@example.com>
2017-10-22 19:32:19 +0900

    [Fix] ボタン内のテキストではなくボタンにリンクつける
    あなたの予約履歴ボタン

--- head.php
+++ head.php
@@ -45,7 +45,7 @@ function output($title){
 EOM;
           //どのページからも見られるのでフルパス
           if (isset($_SESSION['sid'])){
-            echo "<button type=\"button\" class=\"btn btn-default navbar-btn\"><a href=\"/kobai-sys/mypage/history.php\">あなたの予約履歴</a></button>";
+            echo "<button type=\"button\" class=\"btn btn-default navbar-btn\" href=\"/kobai-sys/mypage/history.php\">あなたの予約履歴</a></button>";
             echo "<a href=\"/kobai-sys/mypage/mypage.php\"><p class=\"navbar-text\">ようこそ！".$_SESSION['sid']."さん</p></a>";
           }else {
             echo "<li><a href=\"/kobai-sys/mypage/regist_form.php\"><span class=\"glyphicon glyphicon-user\"></span> 登録</a></li>";
