18f0f1fc6f0150ba915bd0f6f63e7758fe34ef3b
s151003 <hogefuga@example.com>
2017-10-22 15:40:25 +0900

    [Update] DayLimit()追加
    そのIDの商品が何個追加されているか判定
    本日の残りが$remaining

--- mypage/products_list.php
+++ mypage/products_list.php
@@ -4,6 +4,7 @@
 	output("商品ページ");
 	$cat = $_GET['cat'];
 	LoginVerify($_SESSION['sid']);
+	DayLimit("4");
 ?>
 	<!-- レイアウトが崩れないように画像の縦幅を固定 -->
 	<style type="text/css">
@@ -26,15 +27,31 @@
 			$img = "../imgs/not_found.jpg";
 		}
 		ProductCard("$name","$comment","$id","$img","$value");
+
 	}
   echo "</div>";
 	echo "</div>";
 
+function DayLimit($product_id){
+		require("../connect_db.php");
+		$result = mysqli_query($link,"SELECT day_limit FROM `products` WHERE `id` = '$product_id'");
+		$row = mysqli_fetch_assoc($result);
+		$day_limit = $row['day_limit'];
+
+		$today = date("Y-m-d");
+		$result = mysqli_query($link,"SELECT * FROM `yoyaku` WHERE `date` BETWEEN '$today 00:00:00' AND '$today 23:59:59' AND `product`= '$product_id'");
+		while($row = mysqli_fetch_assoc($result)){
+			$today_quantity = $today_quantity + $row['quantity'];
+		}
+		@$remaining = $day_limit - $today_quantity;
+}
+
 function ProductCard($name,$comment,$id,$img,$value){
+
 	  echo <<<EOM
 		<div class="col-md-4">
 	  <div class="panel panel-primary">
-	  <div class="panel-heading">$name</div>
+	  <div class="panel-heading"><div style="float:left;">$name</div><div style="text-align:right;">本日残り *個</div></div>
 	  <div class="panel-body">
 	    <div class="thumbnail">
 	      <img src="$img">
