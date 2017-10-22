c798d68ae4d7e52cf83a407c8b8f69b86c96b15b
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-22 18:29:20 +0900

    [Add] cart.php 追加
    カンマ区切りのaddパラメータ（商品ID,数量）で送られてきた内容をセッションに追加、全て表に表すところまで

---
+++ mypage/cart.php
@@ -0,0 +1,27 @@
+<?php
+require("../head.php");
+output("カート");
+require('../connect_db.php');
+$userid = $_SESSION['sid'];
+if(isset($_GET["add"])){
+  $count = count($_SESSION['cart'][$userid]);
+  $_SESSION['cart'][$userid][$count] = explode(',',$_GET["add"]);
+}
+?>
+<div
+<h1><?php echo $userid ?>さんのカート</h1><hr>
+<table class="table table-condensed">
+  <thead><th>#</th><th>商品</th><th>数量</th><thead>
+    <?php
+    foreach($_SESSION['cart'][$userid] as $key => $value){
+      echo "<tr><td>".$key."</td><td>".$value[0]."</td><td>".$value[1]."</td></tr>";
+    }
+    ?>
+</table>
+
+<?php
+
+//カート全削除
+if(@$_GET["del"] == 1){
+  unset($_SESSION['cart']);
+}
