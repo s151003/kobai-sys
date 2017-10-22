826a1d391af2d6122dd59b9acf9e07d488101189
s151003 <hogefuga@example.com>
2017-10-23 00:57:56 +0900

    [Add] checkout.php カート商品予約確定ページ

---
+++ mypage/checkout.php
@@ -0,0 +1,18 @@
+<?php
+require("../connect_db.php");
+require("../head.php");
+output("予約確定ページ");
+$userid = $_SESSION['sid'];
+$query = "SELECT id FROM member WHERE user_id = '$userid'";
+$result = mysqli_query($link,$query);
+$row = mysqli_fetch_array($result);
+$id = $row['id'];
+
+if(isset($_SESSION['cart'][$userid])){
+  foreach($_SESSION['cart'][$userid] as $key => $value){
+    $datetime = date("Y-m-d H:i:s");
+    $query = "INSERT INTO yoyaku (user_id, product, quantity, date) VALUES ('$id', '$value[0]', '$value[1]','')";
+    $result = mysqli_query($link,$query);
+  };
+  unset($_SESSION['cart']);
+}
