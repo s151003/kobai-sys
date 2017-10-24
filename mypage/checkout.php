22912a4fc5befb82e03b696b0fdb88640d4dc7d5
s151003 <hogefuga@example.com>
2017-10-24 15:54:30 +0900

    [Update] 予約完了時にメッセージを表示

--- mypage/checkout.php
+++ mypage/checkout.php
@@ -10,11 +10,24 @@ $result = mysqli_query($link,$query);
 $row = mysqli_fetch_array($result);
 $id = $row['id'];
 
+echo <<<EOM
+  <div class="panel panel-success">
+  <div class="panel-heading">
+     <span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span> 予約完了！
+  </div>
+  <div class="panel-body">
+EOM;
 if(isset($_SESSION['cart'][$userid])){
   foreach($_SESSION['cart'][$userid] as $key => $value){
     $datetime = date("Y-m-d H:i:s");
     $query = "INSERT INTO yoyaku (user_id, product, quantity, date) VALUES ('$id', '$value[0]', '$value[1]','$datetime')";
     $result = mysqli_query($link,$query);
+    echo $value[0]." * ".$value[1] ."=". $value[0]*$value[1]; 
   };
   unset($_SESSION['cart']);
+  echo <<<EOM
+		予約が完了しました。
+	</div>
+</div>
+EOM;
 }
