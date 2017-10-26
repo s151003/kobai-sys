cf2408adca8ad0f6edaf9281954a687e12d4d381
Unknown <s151003@kashiwanoha.ed.jp>
2017-10-26 20:29:52 +0900

    [Update] 管理者権限付与・解除 ユーザの削除ができるように

--- console/user_list.php
+++ console/user_list.php
@@ -5,9 +5,46 @@ require("../connect_db.php");
 output("ユーザーリスト");
 DataTable("users");
 ?>
+<div class="container">
+<?php
+if (isset($_POST['del'])){
+	$id = $_POST['del'];
+	$query = "DELETE FROM `member` WHERE `id` = '$id'";
+	$result = mysqli_query ($link,$query);
+	if($result){
+		echo "ユーザID".$id."の削除が完了しました";
+	}else{
+		echo "Failed";
+	}
+}
+
+if (isset($_POST['admin'])){
+	$id = $_POST['admin'];
+	$query = "SELECT admin FROM member WHERE id = $id";
+	$result = mysqli_query ($link,$query);
+	$row = mysqli_fetch_array($result);
+	$admin = $row['admin'];
+	switch ($admin){
+		case 0:
+			$admin = 1;
+			break;
+		case 1:
+			$admin = 0;
+			break;
+	}
+		$query = "UPDATE member SET admin = $admin WHERE id = $id";
+		$result = mysqli_query ($link,$query);
+		if($result){
+			echo $admin;
+		}else{
+			echo "Failed";
+		}
+	} 
+
+?>
 <form action="user_list.php" method="post">
 <!-- 表生成開始 -->
-<div class="container">
+
 <table id="users" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
 <thead><tr><th>#</th><th>ユーザー名</th><th>登録日時</th><th>管理権</th><th>切り替え</th><th>削除</th></tr></thead>
 <?php
@@ -21,11 +58,62 @@ while ($row = mysqli_fetch_assoc($result)){
   echo "<td>",$row["user_id"],"</td>";
   echo "<td>".$row["time"]."</td>";
   echo "<td>".$row["admin"]."</td>";
-  echo "<td><button type=\"button\" class=\"btn btn-primary btn-xs\" data-toggle=\"modal\" data-target=\"#admin".$row["id"]."\"><span class=\"glyphicon glyphicon-pencil\"></span></button></td>";
-  echo "<td><button type=\"button\" class=\"btn btn-danger btn-xs\" data-toggle=\"modal\" data-target=\"#delete".$row["id"]."\"><span class=\"glyphicon glyphicon-trash\"></span></button></td>"; //checkbox
+  echo "<td><button type=\"button\" class=\"btn btn-primary btn-xs\" data-toggle=\"modal\" data-target=\"#admin".$row['id']."\"><span class=\"glyphicon glyphicon-pencil\"></span></button></td>";
+  echo "<td><button type=\"button\" class=\"btn btn-danger btn-xs\" data-toggle=\"modal\" data-target=\"#delete".$row['id']."\"><span class=\"glyphicon glyphicon-trash\"></span></button></td>"; //checkbox
   echo "</tr>";
+  ModalSet($row['id'],$row["user_id"],$row['admin']);
  }
- 
   echo "</tbody>";
   echo "</table>";
-  echo "<h4>商品数 $count</h4><hr>";
+  echo "<h4>" .$count. "件</h4><hr>";
+  
+  function ModalSet($id,$name,$admin){
+	    if($admin == 1){
+			$msg = "はく奪しますか？";
+			$aleart = "今後このユーザーは管理者メニューを見ることができません";
+		}else{
+			$msg ="本当に付与しますか？";
+			$aleart ="このユーザーは今後</br><b>「ユーザの管理、すべてのユーザの予約履歴の確認、商品の管理」</b></br>ができるようになります";
+		} 
+  ?>
+
+  	 <div class="modal fade" id="delete<?php echo $id; ?>" tabindex="-1">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+					<h4 class="modal-title">商品の削除</h4>
+				</div>
+					<div class="modal-body">
+					<b><?php echo $name; ?></b> さんの予約履歴は見られなくなります</br>
+					<b> <?php echo $id." : ". $name ?></b> さんのアカウントを本当に削除しますか？
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+					<button type="submit" class="btn btn-danger" name='del' value='<?php echo $id; ?>'>削除</button>
+				</div>
+			</div>
+		</div>
+	  </div>
+	    <div class="modal fade" id="admin<?php echo $id; ?>" tabindex="-1">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+					<h4 class="modal-title">管理者権限切り替え</h4>
+				</div>
+					<div class="modal-body">
+					<?php	?>
+					<b> <?php echo $id." : ".$name; ?></b>  管理者権限を<?php echo $msg; ?></br></br>
+					<?php echo $aleart; ?>
+					 </div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+					<button type="submit" class="btn btn-danger" name='admin' value='<?php echo $id; ?>'>切り替え</button>
+				</div>
+			</div>
+		</div>
+	  </div>
+<?php
+  }
+ ?>
