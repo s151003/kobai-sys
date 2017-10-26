a82559937b2ff105daa96ad17ffd69a52537866b
s151003 <hogefuga@example.com>
2017-10-26 12:48:56 +0900

    [Update] メニューをListにした

--- console/index.php
+++ console/index.php
@@ -1,13 +1,18 @@
 <?php
   require("../head.php");
   AdminVerify($_SESSION['sid']);
-  output("メニュー管理ページ");
+  output("管理ページ");
 ?>
     <div class = "container">
-    <h1>メニュー管理ページ</h1><hr>
-
-    <a class="btn btn-primary" href="menu_add.php">メニュー追加</a></br></br>
-    <a class="btn btn-primary" href="menu_change.php">メニュー管理</a></br></br>
-    <a class="btn btn-primary" href="category_manage.php">カテゴリー管理</a>
-    <a class="btn btn-primary" href="yoyaku_list.php">yoyakulist</a>
-    <a class="btn btn-primary" href="recive_check.php">yoyakulist</a>
+    <h1>管理ページ</h1><hr>
+    	<div class="row">
+        <div class="col-sm-2">
+    <div class="list-group">
+      <a href="menu_add.php" class="list-group-item">メニュー追加</a>
+      <a href="menu_change.php" class="list-group-item">メニュー管理</a>
+      <a href="category_manage.php" class="list-group-item">カテゴリー管理</a>
+      <a href="yoyaku_list.php" class="list-group-item">予約一覧</a>
+      <a href="recive_check.php" class="list-group-item">受取確認</a>
+    </div>
+      </div>
+  </div>
