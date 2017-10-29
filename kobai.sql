5b3828447669aeb41cb08a9ade2fd074db2c2db5
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-29 18:32:26 +0900

    [Update] kobai.sql

--- kobai.sql
+++ kobai.sql
@@ -1,24 +1,27 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017 年 10 朁E27 日 09:43
--- サーバのバージョン： 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: 2017 年 10 朁E29 日 10:27
+-- サーバのバージョン： 10.1.28-MariaDB
+-- PHP Version: 7.0.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `kobai`
 --
+DROP DATABASE IF EXISTS `kobai`;
 CREATE DATABASE IF NOT EXISTS `kobai` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `kobai`;
 
@@ -36,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `id_2` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `category`
@@ -47,8 +50,7 @@ INSERT INTO `category` (`id`, `name`, `comment`) VALUES
 (2, '菓子パン', 'おやつにどうぞ'),
 (3, 'その他', 'おいしいパン'),
 (4, 'マフィン', 'おいしい'),
-(8, 'サンドウィッチ', 'ハムサンドがオススメ'),
-(9, 'パイ', 'さくさく生地');
+(6, 'パイ', 'さくさく生地');
 
 -- --------------------------------------------------------
 
@@ -66,14 +68,14 @@ CREATE TABLE IF NOT EXISTS `member` (
   PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `id_2` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `member`
 --
 
 INSERT INTO `member` (`id`, `user_id`, `password`, `time`, `admin`) VALUES
-(1, 'admin', '$2y$10$9Cy6csfkTVKBSVfycGJrE.RWrJnyqIOlCpwo59KoeA1Y56YUMqefG', '2017-10-12 00:00:00', 1);
+(1, 'admin', '$2y$10$9Cy6csfkTVKBSVfycGJrE.RWrJnyqIOlCpwo59KoeA1Y56YUMqefG', '2017-10-14 06:34:22', 1);
 
 -- --------------------------------------------------------
 
@@ -83,8 +85,7 @@ INSERT INTO `member` (`id`, `user_id`, `password`, `time`, `admin`) VALUES
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(255) NOT NULL 
-  _INCREMENT,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `value` int(255) NOT NULL,
   `dis_day` int(255) NOT NULL,
@@ -95,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `comment` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `products`
@@ -108,7 +109,10 @@ INSERT INTO `products` (`id`, `name`, `value`, `dis_day`, `dis_value`, `day_limi
 (4, 'ラスク', 50, 7, 0, 20, 0, 3, '牛乳を飲みながら食べたい'),
 (5, '食パン', 120, 4, 15, 60, 0, 3, 'ジャムをお持ちの方はこちらをどうぞ'),
 (6, 'くるみパン', 120, 3, 50, 30, 0, 1, 'なつかしい中国を思い出す味'),
-(7, 'オレンジパン', 18, 7, 0, 2, 0, 9, 'おいしいおいしい');
+(7, 'オレンジパン', 18, 7, 0, 2, 0, 9, 'おいしいおいしい'),
+(8, 'ホイップあんぱん', 100, 7, 0, 10, 0, 2, 'ホイップとあんこの定番コンビ'),
+(9, 'チャプチェ', 100, 7, 0, 10, 0, 1, '韓国料理'),
+(10, 'クリームパン', 80, 7, 0, 10, 0, 2, '安い！迷ったらコレ！');
 
 -- --------------------------------------------------------
 
@@ -123,11 +127,10 @@ CREATE TABLE IF NOT EXISTS `yoyaku` (
   `product` varchar(255) NOT NULL,
   `quantity` varchar(255) NOT NULL,
   `date` datetime NOT NULL,
-  `status` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1000 ;
-
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
