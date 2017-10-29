dec905c28f98fa157ed113718792c308e69afddc
s151003 <hogefuga@example.com>
2017-10-29 20:03:20 +0900

    Revert "[Update] カートに追加した商品を削除できるように"
    This reverts commit e0c45e947b3d33872f0944513bc1ea2972efeaf6.

--- mypage/login.php
+++ mypage/login.php
@@ -1,33 +1,20 @@
 <?php
 require("../head.php");
-// 先に$_GET['err']を判定して$_SESSION['sid']をクリアしておかないと
-// 右上のアイコンが「ようこそ○さん」のままになる。
-if (isset($_GET['err']) && $_GET['err'] == "logout"){
-    session_unset($_SESSION['sid']);
-}
-
 output("ログイン");
-?>
-<div class="container">
-<?php
+
 //エラー表示
 if (isset($_GET['err'])){
+  echo "<div class=\"panel panel-danger\"><div class=\"panel-heading\">ログインエラー</div>";
   echo "<div class=\"panel-body\">";
-  $err_message = "<div class=\"panel panel-danger\"><div class=\"panel-heading\"> %s </div>";
   switch($_GET['err']){
-  case "logout":
-      echo sprintf($err_message, "ログアウトしました。 <a href=\"login.php\"> 再ログイン</a>");
-      break;
-  case "login":
-      echo sprintf($err_message,
-                   "ログインしていないかセッションの有効期限が切れたため、
-                    もう一度ログインしてください。<a href=\"login.php\"> 再ログイン</a>");
-      break;
-  case "you_dont_have_permission":
-      echo sprintf($err_message, "アクセスに必要な権限がありません。");
-      break;
-  default:
-      echo sprintf($err_message, "ログインエラー");
+    case "logout":
+      session_unset($_SESSION['sid']);
+      echo "ログアウトしました <a href=\"login.php\"> 再ログイン</a>";
+      exit();
+    case "login":
+      echo "ログインしていないかセッションの有効期限が切れたため、もう一度ログインしてください。";
+    case "you_dont_have_permission":
+      echo "アクセスに必要な権限がありません";
   }
   echo "</div></div>";
 }
@@ -37,13 +24,13 @@ if (isset($_GET['err'])){
   <h2>ログイン</h2>
   <hr>
   <div class="form-froup">
-    <label>ID</label></br>
-    <input required type="text" name="id" maxlength="100" class="form-control"></br>
+    <label>ID</label><br>
+    <input required type="text" name="id" maxlength="100" class="form-control"><br>
   </div>
 
   <div class="form-froup">
-    <label>password</label></br>
-    <input required type="password" name="password" maxlength="100" class="form-control"></br>
+    <label>password</label><br>
+    <input required type="password" name="password" maxlength="100" class="form-control"><br>
   </div>
     <input class="btn btn-primary" type="submit" name="login" value="ログイン">
 </div>
