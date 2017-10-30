7e6ff239082ff4a7e1e392c17d05840826c211ef
Kohei Ito <s151003@kashiwanoha.ed.jp>
2017-10-30 15:00:11 +0900

    [Update] index.php

--- index.php
+++ index.php
@@ -10,9 +10,5 @@
 	      <p><a class="btn btn-primary btn-lg" href="mypage/login.php" role="button">ログイン</a> <a class="btn btn-primary btn-lg" href="mypage/regist_form.php" role="button">新規登録</a></p>
     </div>
     <hr>
-    <div class="alert alert-info">
-      <strong>ようこそ！</strong> 柏の葉高校購買予約システム
-    </div><br>
-    HTML PHP MySQL Bootstrap Datat
   </div>
 </body>
