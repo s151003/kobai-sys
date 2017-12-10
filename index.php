30871bd7f1755faee753b5de17324c080fc059bd
s151003 <hogefuga@example.com>
2017-12-10 15:17:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <div class="container">
     <div class="jumbotron">
 	     <h1>購買予約システムへようこそ</h1>
-	      <p>柏の葉高校購買予約システム</p>
+	      <p>購買予約システム</p>
 	      <p><a class="btn btn-primary btn-lg" href="mypage/login.php" role="button">ログイン</a> <a class="btn btn-primary btn-lg" href="mypage/regist_form.php" role="button">新規登録</a></p>
     </div>
     <hr>
